146353f65d3e528a3e93db13ced282b36a93ae76
David Lu <david@adroitcreations.com>
2021-10-14 00:08:54 +1300

    Tweak dynamodb

--- main.tf
+++ main.tf
@@ -60,9 +60,9 @@ resource "aws_s3_bucket_public_access_block" "terraform_state_access" {
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraformlock"
-  read_capacity  = 0
-  write_capacity = 0
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "LockID"
 
